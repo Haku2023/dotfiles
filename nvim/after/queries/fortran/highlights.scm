5d5b61e1f9bc8f1c2e8532db2dbf515cd7270350
haku_micro <baihdong@gmail.com>
2026-01-05 09:16:02 +0000

    SS- add fortran subroutine hl

--- nvim/after/queries/fortran/highlights.scm
+++ nvim/after/queries/fortran/highlights.scm
@@ -1,4 +1,13 @@
-;; extends
+; extends
 
+; Make function calls win vs variable highlight
+; Subroutine calls
+(subroutine_call
+  subroutine: (identifier) @function.call
+  (#set! priority 110))
 ; Remove character from @function.builtin and add to @type.builtin
 "character" @type.builtin
+
+
+
+
