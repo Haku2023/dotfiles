bae6eda6cb349ed8177d356d9d9ec0cf393f2bc8
haku_micro <baihdong@gmail.com>
2026-02-13 00:10:20 +0000

    SS-improve zsh, add indent keymaps

--- nvim/lua/haku/core/keymaps.lua
+++ nvim/lua/haku/core/keymaps.lua
@@ -11,6 +11,9 @@ keymap.set("x", "s", "<ESC>", { desc = "Exit visual mode with s" })
 keymap.set("i", "<C-f>", "<Right>", { desc = "forward in insertmode" })
 keymap.set("i", "<C-b>", "<Left>", { desc = "backward in insertmode" })
 keymap.set("i", "hh", "<C-w>", { desc = "delete words in insertmode" })
+-- indent, unindent
+keymap.set({ "n", "x" }, "<leader>,,", "<<", { desc = "Indent" })
+keymap.set({ "n", "x" }, "<leader>..", ">>", { desc = "Unindent" })
 
 -- keymap.set({ "n" }, "<C-t>", "<cmd>set wrap<CR>", { desc = "wrap the lines" })
 -- keymap.set({ "i" }, "<C-t>", "<ESC>:set wrap<CR>", { desc = "wrap the lines" })
